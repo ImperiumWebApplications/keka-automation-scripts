13c809f641a520ca077e9bd7c7de71f1d24b2ce9
Aditya Singh <adityasingh220598@gmail.com>
2023-09-26 11:16:15 +0530

    Fix: updating the scripts

--- clockin.js
+++ clockin.js
@@ -1,66 +1,41 @@
 const puppeteer = require("puppeteer");
-const fs = require("fs");
-const path = require("path");
 
-const userDataDir = "/home/ubuntu/my_chrome_data";
-const singletonLockFile = path.join(userDataDir, "SingletonLock");
+const userDataDirPath = "/Users/imperium/Documents/my_chrome_data";
 
-try {
-  fs.unlinkSync(singletonLockFile);
-} catch (error) {
-  if (error.code !== "ENOENT") {
-    console.log(`Error deleting SingletonLock file: ${error}`);
-  }
-}
-
-const run = async () => {
-  let browser;
+(async () => {
   try {
-    browser = await puppeteer.launch({
+    const browser = await puppeteer.launch({
       headless: "new",
-      userDataDir,
+      userDataDir: userDataDirPath,
     });
+
     const page = await browser.newPage();
-    await page.goto("https://ibexlabs.keka.com/");
 
-    await page.waitForTimeout(20000);
-
-    await page.evaluate(() => {
-      const buttons = document.querySelectorAll("button");
-      for (let button of buttons) {
-        if (button.textContent === "Web Clock-In") {
-          button.click();
-          break;
-        }
-      }
+    await page.goto("https://ibexlabs.keka.com/#/home/dashboard", {
+      waitUntil: "networkidle2",
     });
 
-    await page.waitForTimeout(10000);
-
-    await page.evaluate(() => {
-      const buttons = document.querySelectorAll("button");
-      for (let button of buttons) {
-        if (button.textContent === "Confirm") {
-          button.click();
-          break;
-        }
-      }
-    });
-
-    await page.waitForTimeout(10000);
-  } catch (error) {
-    console.log(`Error occured within clockin script ${error}`);
-  } finally {
-    if (browser) {
-      try {
-        await browser.close();
-      } catch (closeError) {
-        console.log(
-          `An error occurred while closing the browser: ${closeError}`
-        );
-      }
+    let buttons = await page.$x("//button[contains(text(), 'Web Clock-In')]");
+    if (buttons.length > 0) {
+      await buttons[0].click();
+    } else {
+      console.log("Button not found");
     }
-  }
-};
 
-run();
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+
+    buttons = await page.$x("//button[contains(text(), 'Confirm')]");
+    if (buttons.length > 0) {
+      await buttons[0].click();
+    } else {
+      console.log("Button not found");
+    }
+
+    // Adding a 5-second delay to observe the browser before it closes
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+
+    await browser.close();
+  } catch (error) {
+    console.error("An error occurred:", error);
+  }
+})();
