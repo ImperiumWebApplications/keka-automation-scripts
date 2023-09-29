10264c84bb499de21b37f308c6754159ee6a833f
Aditya Singh <adityasingh220598@gmail.com>
2023-09-29 20:42:32 +0530

    Fix: migrating to playwright

--- clockout.js
+++ clockout.js
@@ -3,50 +3,45 @@ const { chromium } = require("playwright");
 const userDataDirPath = "/home/ubuntu/my_chrome_data";
 
 (async () => {
-  let browser;
   let context;
   try {
     context = await chromium.launchPersistentContext(userDataDirPath, {
       headless: true,
     });
-    browser = context.browser();
     const page = await context.newPage();
 
     await page.goto("https://ibexlabs.keka.com/#/home/dashboard");
 
-    await page.waitForTimeout(20000); // Replaced the Promise + setTimeout with waitForTimeout
+    await page.waitForTimeout(20000); // Wait for page to load
 
-    let buttons = await page.$$("text=Clock-out"); // Replaced the XPath selector with a text selector
-    if (buttons.length > 0) {
-      await buttons[0].click();
-    } else {
-      console.log("Button not found");
+    // Function to find and click a button based on its text content
+    async function clickButton(text) {
+      const button = await page.evaluateHandle((text) => {
+        const buttons = Array.from(document.querySelectorAll("button"));
+        return buttons.find((button) => button.textContent.includes(text));
+      }, text);
+
+      if (button) {
+        await button.click();
+      } else {
+        console.log(`Button with text "${text}" not found`);
+      }
     }
+
+    await clickButton("Clock-out"); // Click the first Clock-out button
+    await page.waitForSelector("text=Clock-out", { timeout: 5000 }); // Wait for a certain element or text to appear
+
+    await clickButton("Clock-out"); // Click the second Clock-out button
+    await page.waitForSelector("text=Confirm", { timeout: 5000 }); // Wait for a certain element or text to appear
+
+    await clickButton("Confirm"); // Click the Confirm button
 
     await page.waitForTimeout(5000);
 
-    buttons = await page.$$("text=Clock-out"); // Replaced the XPath selector with a text selector
-    if (buttons.length > 0) {
-      await buttons[0].click();
-    } else {
-      console.log("Button not found");
-    }
-
-    await page.waitForTimeout(5000);
-
-    buttons = await page.$$("text=Confirm"); // Replaced the XPath selector with a text selector
-    if (buttons.length > 0) {
-      await buttons[0].click();
-    } else {
-      console.log("Button not found");
-    }
-
-    await page.waitForTimeout(5000);
-
-    await browser.close();
+    await context.close();
   } catch (error) {
-    if (browser) {
-      await browser.close();
+    if (context) {
+      await context.close();
     }
     console.error("An error occurred:", error);
   }
