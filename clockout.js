96d18854edaeecaa286eb8ae54fd088e1cc38924
Aditya Singh <adityasingh220598@gmail.com>
2023-09-29 20:05:57 +0530

    Fix: normalising space

--- clockout.js
+++ clockout.js
@@ -14,29 +14,35 @@ let browser;
     await page.goto("https://ibexlabs.keka.com/#/home/dashboard");
     await page.setDefaultNavigationTimeout(60000);
     console.log("Finding first button");
-    await page.waitForXPath("//button[contains(text(), 'Clock-out')]", {
-      timeout: 30000,
-    });
-    let buttons = await page.$x("//button[contains(text(), 'Clock-out')]");
+    await page.waitForXPath(
+      "//button[contains(normalize-space(text()), 'Clock-out')]",
+      { timeout: 30000 }
+    );
+
+    let buttons = await page.$x("//button[contains(normalize-space(text()), 'Clock-out')]");
     if (buttons.length > 0) {
       await buttons[0].click();
     } else {
       console.log("Button not found");
     }
     console.log("Finding second button");
-    await page.waitForXPath("//button[contains(text(), 'Clock-out')]", {
-      timeout: 30000,
-    });
-    buttons = await page.$x("//button[contains(text(), 'Clock-out')]");
+    await page.waitForXPath(
+      "//button[contains(normalize-space(text()), 'Clock-out')]",
+      { timeout: 30000 }
+    );
+
+    buttons = await page.$x("//button[contains(normalize-space(text()), 'Clock-out')]");
     if (buttons.length > 0) {
       await buttons[0].click();
     } else {
       console.log("Button not found");
     }
 
-    await page.waitForXPath("//button[contains(text(), 'Confirm')]", {
-      timeout: 30000,
-    });
+    await page.waitForXPath(
+      "//button[contains(normalize-space(text()), 'Clock-out')]",
+      { timeout: 30000 }
+    );
+
     buttons = await page.$x("//button[contains(text(), 'Confirm')]");
     if (buttons.length > 0) {
       await buttons[0].click();
