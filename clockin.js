0957f3c8dd7ace0859eb4f64a15aace375938aac
Aditya Singh <adityasingh220598@gmail.com>
2023-11-15 10:23:23 +0530

    Fix: adding retry mechanism to the scripts

--- clockin.js
+++ clockin.js
@@ -1,8 +1,9 @@
 const { chromium } = require("playwright");
 
 const userDataDirPath = "/Users/imperium/Documents/my_chrome_data";
+const maxRetries = 3;
 
-(async () => {
+async function runClockInScript(retryCount = 0) {
   let context;
   try {
     context = await chromium.launchPersistentContext(userDataDirPath, {
@@ -11,35 +12,39 @@ const userDataDirPath = "/Users/imperium/Documents/my_chrome_data";
     const page = await context.newPage();
 
     await page.goto("https://ibexlabs.keka.com/#/home/dashboard");
+    await page.waitForTimeout(20000);
 
-    await page.waitForTimeout(20000); // Wait for page to load
-
-    // Function to find and click a button based on its text content
     async function clickButton(text) {
       const button = await page.evaluateHandle((text) => {
         const buttons = Array.from(document.querySelectorAll("button"));
         return buttons.find((button) => button.textContent.includes(text));
       }, text);
 
-      if (button) {
+      if (button && typeof button.click === "function") {
         await button.click();
       } else {
-        console.log(`Button with text "${text}" not found`);
+        console.log(`Button with text "${text}" not found or not clickable`);
       }
     }
 
-    await clickButton("Web Clock-In"); // Click the first Clock-out button
-    await page.waitForSelector("text=Confirm", { timeout: 5000 }); // Wait for a certain element or text to appear
+    await clickButton("Web Clock-In");
+    await page.waitForSelector("text=Confirm", { timeout: 5000 });
 
-    await clickButton("Confirm"); // Click the Confirm button
+    await clickButton("Confirm");
 
     await page.waitForTimeout(5000);
-
     await context.close();
   } catch (error) {
+    console.error("An error occurred:", error);
+    if (retryCount < maxRetries) {
+      console.log(`Retrying... Attempt ${retryCount + 1}`);
+      await runClockInScript(retryCount + 1);
+    }
+  } finally {
     if (context) {
       await context.close();
     }
-    console.error("An error occurred:", error);
   }
-})();
+}
+
+runClockInScript();
