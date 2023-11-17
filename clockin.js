1245ec34de15a2f6aeb17037a066bf33f5f2f99a
Aditya Singh <adityasingh220598@gmail.com>
2023-11-17 10:15:27 +0530

    Revert "Fix: adding retry mechanism to the scripts"
    This reverts commit 0957f3c8dd7ace0859eb4f64a15aace375938aac.

--- clockin.js
+++ clockin.js
@@ -1,9 +1,8 @@
 const { chromium } = require("playwright");
 
 const userDataDirPath = "/Users/imperium/Documents/my_chrome_data";
-const maxRetries = 3;
 
-async function runClockInScript(retryCount = 0) {
+(async () => {
   let context;
   try {
     context = await chromium.launchPersistentContext(userDataDirPath, {
@@ -12,39 +11,35 @@ async function runClockInScript(retryCount = 0) {
     const page = await context.newPage();
 
     await page.goto("https://ibexlabs.keka.com/#/home/dashboard");
-    await page.waitForTimeout(20000);
 
+    await page.waitForTimeout(20000); // Wait for page to load
+
+    // Function to find and click a button based on its text content
     async function clickButton(text) {
       const button = await page.evaluateHandle((text) => {
         const buttons = Array.from(document.querySelectorAll("button"));
         return buttons.find((button) => button.textContent.includes(text));
       }, text);
 
-      if (button && typeof button.click === "function") {
+      if (button) {
         await button.click();
       } else {
-        console.log(`Button with text "${text}" not found or not clickable`);
+        console.log(`Button with text "${text}" not found`);
       }
     }
 
-    await clickButton("Web Clock-In");
-    await page.waitForSelector("text=Confirm", { timeout: 5000 });
+    await clickButton("Web Clock-In"); // Click the first Clock-out button
+    await page.waitForSelector("text=Confirm", { timeout: 5000 }); // Wait for a certain element or text to appear
 
-    await clickButton("Confirm");
+    await clickButton("Confirm"); // Click the Confirm button
 
     await page.waitForTimeout(5000);
+
     await context.close();
   } catch (error) {
-    console.error("An error occurred:", error);
-    if (retryCount < maxRetries) {
-      console.log(`Retrying... Attempt ${retryCount + 1}`);
-      await runClockInScript(retryCount + 1);
-    }
-  } finally {
     if (context) {
       await context.close();
     }
+    console.error("An error occurred:", error);
   }
-}
-
-runClockInScript();
+})();
--- clockout.js
+++ clockout.js
@@ -1,9 +1,8 @@
 const { chromium } = require("playwright");
 
 const userDataDirPath = "/Users/imperium/Documents/my_chrome_data";
-const maxRetries = 3;
 
-async function runClockOutScript(retryCount = 0) {
+(async () => {
   let context;
   try {
     context = await chromium.launchPersistentContext(userDataDirPath, {
@@ -12,42 +11,38 @@ async function runClockOutScript(retryCount = 0) {
     const page = await context.newPage();
 
     await page.goto("https://ibexlabs.keka.com/#/home/dashboard");
+
     await page.waitForTimeout(20000); // Wait for page to load
 
+    // Function to find and click a button based on its text content
     async function clickButton(text) {
       const button = await page.evaluateHandle((text) => {
         const buttons = Array.from(document.querySelectorAll("button"));
         return buttons.find((button) => button.textContent.includes(text));
       }, text);
 
-      if (button && typeof button.click === "function") {
+      if (button) {
         await button.click();
       } else {
-        console.log(`Button with text "${text}" not found or not clickable`);
+        console.log(`Button with text "${text}" not found`);
       }
     }
 
-    await clickButton("Clock-out");
-    await page.waitForSelector("text=Clock-out", { timeout: 5000 });
+    await clickButton("Clock-out"); // Click the first Clock-out button
+    await page.waitForSelector("text=Clock-out", { timeout: 5000 }); // Wait for a certain element or text to appear
 
-    await clickButton("Clock-out");
-    await page.waitForSelector("text=Confirm", { timeout: 5000 });
+    await clickButton("Clock-out"); // Click the second Clock-out button
+    await page.waitForSelector("text=Confirm", { timeout: 5000 }); // Wait for a certain element or text to appear
 
-    await clickButton("Confirm");
+    await clickButton("Confirm"); // Click the Confirm button
 
     await page.waitForTimeout(5000);
+
     await context.close();
   } catch (error) {
-    console.error("An error occurred:", error);
-    if (retryCount < maxRetries) {
-      console.log(`Retrying... Attempt ${retryCount + 1}`);
-      await runClockOutScript(retryCount + 1);
-    }
-  } finally {
     if (context) {
       await context.close();
     }
+    console.error("An error occurred:", error);
   }
-}
-
-runClockOutScript();
+})();
